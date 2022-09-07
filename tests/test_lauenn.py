202b8695c5648dacb978e0781c32ef58413455cb
Ravi <42536488+ravipurohit1991@users.noreply.github.com>
2022-09-07 23:14:32 +0200

    cosmetic

--- tests/test_lauenn.py
+++ tests/test_lauenn.py
@@ -13,3 +13,18 @@ import skimage
 import fabio
 import networkx
 import tqdm
+
+import pkg_resources
+
+print("Lauetoolsnn :", pkg_resources.require("lauetoolsnn")[0].version)
+print("scipy :", pkg_resources.require("scipy")[0].version)
+print("numpy :", pkg_resources.require("numpy")[0].version)
+print("h5py :", pkg_resources.require("h5py")[0].version)
+print("keras :", pkg_resources.require("keras")[0].version)
+print("tensorflow :", pkg_resources.require("tensorflow")[0].version)
+print("PyQt5 :", pkg_resources.require("PyQt5")[0].version)
+print("sklearn :", pkg_resources.require("sklearn")[0].version)
+print("skimage :", pkg_resources.require("skimage")[0].version)
+print("fabio :", pkg_resources.require("fabio")[0].version)
+print("networkx :", pkg_resources.require("networkx")[0].version)
+print("tqdm :", pkg_resources.require("tqdm")[0].version)
