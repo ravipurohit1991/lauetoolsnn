b1229cc5089f65e65ced8200cfdde86ee087505c
Ravi <42536488+ravipurohit1991@users.noreply.github.com>
2022-09-10 21:41:42 +0200

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 setuptools.setup(
     name="lauetoolsnn",
     
-    version="3.0.67",
+    version="3.0.68",
 
     author="Ravi raj purohit PURUSHOTTAM RAJ PUROHIT",
     
