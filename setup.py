194951ff2b0bd2fb098d02cab3f019649dc42fcf
Ravi <42536488+ravipurohit1991@users.noreply.github.com>
2022-09-10 11:18:37 +0200

    update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     url="https://github.com/ravipurohit1991/lauetoolsnn",
     
     setup_requires=['matplotlib', 'Keras', 'scipy','numpy', 'h5py', 'tensorflow', 'PyQt5', 'scikit-learn', 'fabio', 'networkx', 'scikit-image', 'tqdm'],
-#    install_requires=['matplotlib>=3.4.2', 'Keras>=2.7.0', 'scipy>=1.7.0','numpy>=1.18.5', 'h5py>=3.1', 'tensorflow>=2.7.0', 'PyQt5>=5.9', 'scikit-learn>=0.24.2', 'fabio>=0.11.0', 'networkx>=2.6.3', 'scikit-image>=0.18.0','tqdm>=4.60.0'],
+    install_requires=['matplotlib>=3.4.2', 'Keras>=2.7.0', 'scipy>=1.7.0','numpy>=1.18.5', 'h5py>=3.1', 'tensorflow>=2.7.0', 'PyQt5>=5.9', 'scikit-learn>=0.24.2', 'fabio>=0.11.0', 'networkx>=2.6.3', 'scikit-image>=0.18.0','tqdm>=4.60.0'],
 
 
     entry_points={
@@ -39,6 +39,7 @@ setuptools.setup(
                  },
                  
     classifiers=[
+                    "Programming Language :: Python :: 3.6",
                     "Programming Language :: Python :: 3.7",
                     "Programming Language :: Python :: 3.8",
                     "Programming Language :: Python :: 3.9",
@@ -49,5 +50,5 @@ setuptools.setup(
                     "License :: OSI Approved :: MIT License "
                 ],
                 
-    python_requires='~=3.7',
+    python_requires='~=3.6',
 )
