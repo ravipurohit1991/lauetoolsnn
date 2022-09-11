7e8aea03e03ac3c162b90f44cf1b035fbc5775f8
Ravi <42536488+ravipurohit1991@users.noreply.github.com>
2022-09-11 22:48:49 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'lauetoolsnn'
+copyright = '2022, Ravi PURUSHOTTAM'
+author = 'ravipurohit1991'
 
 release = '0.1'
 version = '0.1.0'
